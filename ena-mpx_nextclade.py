3a1b296d346db78168be03d4cffd8063c595661e
Khadim Gueye <89599484+KhadimGueyeKGY@users.noreply.github.com>
2022-11-24 10:16:24 +0000

    Update ena-mpx_nextclade.py

--- ena-mpx_nextclade.py
+++ ena-mpx_nextclade.py
@@ -7,7 +7,7 @@
 
 import os
 import pandas as pd
-from datetime import  datetime
+from datetime import  datetime , date
  
 
 
@@ -16,9 +16,7 @@ os.system("cd packages/ena-mpx-prep/ ; python metadata_processing.py")
 os.system("mkdir -p packages/ena-mpx-prep/data/seq")
 matadata = pd.read_csv("packages/ena-mpx-prep/data/ENA_Search.tsv",sep="\t")
 id = matadata.accession
-contry = matadata.country
-date = matadata.collection_date
-date_public = matadata.first_public
+
 
 # #**************************************** Downloading  ....
 print('\n\n> Downloading Fastas ... [ '+str(datetime.now())+" ]")
@@ -33,3 +31,24 @@ print('\n\n> Running Nextclade analysis pipeline ... [ '+str(datetime.now())+" ]
 os.system("nextclade run   --input-dataset 'packages/dataset/MPXV/'   --output-csv 'output/nextclade.csv' --output-tsv 'output/nextclade.tsv' --output-tree 'output/tree.json' packages/ena-mpx-prep/data/sequences.fasta")
 
 print('\033[1;32m \n\n> Done ... [ '+str(datetime.now())+" ] \n\n")
+
+
+###################################### Generat of the public_sequence_pango_lineages[date] file. 
+
+input_file = open("output/nextclade.tsv","r").read().split("\n")
+date= date.today()
+new_file = "../public_sequence_pango_lineages_"+str(date.day)+str(date.month)+str(date.year)+".csv"
+output_file = open(new_file, "w")
+output_file.write("accession,clade,lineage\n")
+for i in range(1,len(input_file)):
+    if input_file[i] !="":
+        acc =input_file[i].split("\t")[0].split("|")[1]
+        clade=input_file[i].split("\t")[1]
+        lineage = input_file[i].split("\t")[3]
+        output_file.write(str(acc)+","+str(clade)+","+str(lineage)+"\n")
+output_file.close()
+ 
+
+
+
+os.system("ln -sf  "+new_file+"  ../public_sequence_pango_lineages.csv")
